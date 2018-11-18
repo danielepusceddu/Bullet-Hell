d5d15561d8f9101781a27c44006b4117d316209d
pusceddudaniele <43929743+pusceddudaniele@users.noreply.github.com>
2018-11-18 18:37:24 +0100

    AIComponent: Use rand and chrono instead of stdlib

--- src/AIComponent/AIComponent.cpp
+++ src/AIComponent/AIComponent.cpp
@@ -1,8 +1,11 @@
 #include "AIComponent.hpp"
-#include <iostream>
+#include <random>
+#include <chrono>
 
 //Constructor
-AIComponent::AIComponent(){
+AIComponent::AIComponent():
+randEng{static_cast<unsigned long>(std::chrono::high_resolution_clock::now().time_since_epoch().count())}
+{
     x_destination = -1;
 }
 
@@ -19,12 +22,16 @@ void AIComponent::update(Ship& ship, float delta){
 }
 
 void AIComponent::getNewDestination(Ship& ship){
+    //Calculating min x and max x
     sf::FloatRect bounds = ship.getBounds();
     sf::FloatRect shipRect = ship.getGlobalRect();
+    int minX = static_cast<int>(bounds.left);
+    int maxX = static_cast<int>(bounds.width - shipRect.width - 1);
 
-    x_destination = rand() % (int)(bounds.width - shipRect.width - 1) + bounds.left;
+    //Getting random x
+    std::uniform_int_distribution<int> xPicker{minX, maxX};
+    x_destination = xPicker(randEng);
     destGreaterThanX = x_destination >= shipRect.left;
-
 }
 
 void AIComponent::moveShip(Ship& ship, float delta){
--- src/AIComponent/AIComponent.hpp
+++ src/AIComponent/AIComponent.hpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "../Ship/Ship.hpp"
 #include <functional>
+#include <random>
 
 class AIComponent{
     public:
@@ -9,9 +10,14 @@ class AIComponent{
     void update(Ship& ship, float delta);
 
     private:
+    //Random Engine
+    std::default_random_engine randEng;
+
+    //Destination tracking
     int x_destination;
     bool destGreaterThanX;
 
+    //Private methods
     void moveShip(Ship& ship, float delta);
     bool reachedDestination(Ship& ship);
     void getNewDestination(Ship& ship);
